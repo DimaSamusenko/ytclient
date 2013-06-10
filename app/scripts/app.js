a0986a865c4d2a29c7ec306b46c3082d4c1ba682
DimaSamusenko <dima@samusenka.com>
2013-06-10 12:54:37 +0300

    navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,9 @@ angular.module('ytclientApp', ['ngResource']).config(function ($routeProvider) {
         $routeProvider.when('/', {
                 templateUrl: 'views/main.html',
                 controller : 'MainCtrl'
+            }).when('/category/:channelId', {
+                templateUrl: 'views/main.html',
+                controller : 'MainCtrl'
             }).otherwise({
                 redirectTo: '/'
             });
